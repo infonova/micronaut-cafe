ffc7eb32197894ecb20281b02327427c499e3141
Bernhard Vollnhofer <bernhard.vollnhofer@veroo.io>
2021-02-03 12:15:17 +0100

    initial commit

---
+++ src/test/java/io/bervol/MicronautCafeTest.java
@@ -0,0 +1,21 @@
+package io.bervol;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class MicronautCafeTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}

e3b12940eebd941377c052d1001a0fb9cdb1d32b
Bernhard Vollnhofer <bernhard.vollnhofer@veroo.io>
2021-02-04 10:43:19 +0100

    add logging

--- src/main/java/io/bervol/Application.java
+++ src/main/java/io/bervol/Application.java
@@ -2,11 +2,15 @@ package io.bervol;
 
 import io.micronaut.runtime.Micronaut;
 
+import java.lang.management.ManagementFactory;
+
 public class Application {
 
     public static void main(String[] args) {
         Micronaut.build(args)
                 .banner(false)
                 .start();
+        long jvmUpTime = ManagementFactory.getRuntimeMXBean().getUptime();
+        System.out.printf("JVM running since %d ms%n", jvmUpTime);
     }
 }

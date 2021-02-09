80ab3b6f6a72634a2380266e03c6a58a33175258
Bernhard Vollnhofer <bernhard.vollnhofer@veroo.io>
2021-02-09 15:29:22 +0100

    use logger

--- src/main/java/io/bervol/Application.java
+++ src/main/java/io/bervol/Application.java
@@ -1,16 +1,20 @@
 package io.bervol;
 
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.management.ManagementFactory;
 
 public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         Micronaut.build(args)
                 .banner(false)
                 .start();
         long jvmUpTime = ManagementFactory.getRuntimeMXBean().getUptime();
-        System.out.printf("JVM running since %d ms%n", jvmUpTime);
+        logger.info("JVM running since {}", jvmUpTime);
     }
 }
